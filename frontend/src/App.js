a3080acf24a9e8994c40b9f202b88577b1bfabef
Fabphokie <mmaphokengsenne@gmail.com>
2025-03-04 12:39:57 +0200

    added the correctional-service page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MissingPerson from './pages/MissingPerson';
+import CorrectionalService from './pages/CorrectionalService';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/missing-person" element={<MissingPerson />} />
+        <Route path="/correctional-service" element={<CorrectionalService />} />
       </Routes>
     </Router>
   );
