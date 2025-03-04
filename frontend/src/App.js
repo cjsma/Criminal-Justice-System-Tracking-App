362a96d90140b46d7d915a32721673ab80fa3bfd
Sanzamgudu <153663155+Sanzamgudu@users.noreply.github.com>
2025-03-04 17:09:29 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MissingPerson from './pages/MissingPerson';
 import CorrectionalService from './pages/CorrectionalService';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
